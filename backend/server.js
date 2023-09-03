bc4ea94f7807322177a03ef6564578824f965b7e
Bibek Bhusal <iambibker@gmail.com>
2023-09-03 11:17:26 +0545

    Res Check with Dummy Chat Data

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/dummyData");
 const app= express();
-
+dotenv.config()
 
 app.get("/", (req,res)=> {
     res.send("API is Running");
 })
 
+app.get("/api/chats",(req,res)=> {
+    res.send(chats)
+ })
 
+ app.get("/api/chats/:id",(req,res)=> {
+    const {id}= req.params;
+    const singleChat= chats.find((c)=>c._id===id)
+    res.send(singleChat)
+ })
+  
+
+ const PORT = process.env.PORT || 5000;
+ 
 app.listen(
-    5000, 
-    console.log("App is running on Port 5000")
+    PORT, 
+    console.log(`App is running on Port ${PORT}`)
     );
