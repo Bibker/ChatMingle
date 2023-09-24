586927e21bc2c5666ffbb5c0d3fc86bf5cbd9e78
Bibek Bhusal <iambibker@gmail.com>
2023-09-24 20:49:58 +0545

    Create Group Chat Api Created

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -67,4 +67,34 @@ const fetchChat = asyncHandler(async( req, res) => {
         throw new Error(error.message);
     }
 })
-module.exports= {accessChat,fetchChat}
+
+const createGroupChat = asyncHandler( async(req,res)=> {
+    if(!req.body.users || !req.body.name)
+    {
+        return res.status(400).send({message:"Please Fill all the Fields"});
+    }
+
+    var users = JSON.parse(req.body.users);
+    if(users.length < 2) {
+        return res.status(400).send({message:"Add more users"});
+    }
+    users.push(req.user);
+    try {
+        const groupChat = await Chat.create({
+            chatName: req.body.name,
+            users:users,
+            isGroupChat:true,
+            groupAdmin: req.user,       
+        });
+
+        const fullGroupChat= await Chat.findOne({_id:groupChat._id})
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+        res.status(200).json(fullGroupChat);
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+
+})
+module.exports= {accessChat,fetchChat, createGroupChat}
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { accessChat, fetchChat } = require("../controllers/chatControllers");
+const { accessChat, fetchChat, createGroupChat } = require("../controllers/chatControllers");
 const {protect} = require('../middleware/authMiddleware');
 
 const router= express.Router();
 
 router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChat);
-// router.route('/group').post(protect, createGroupChat);
+router.route('/group').post(protect, createGroupChat);
 // router.route('/rename').put(protect, renameGroupChat);
 // router.route('/group-remove').put(protect, removeFromGroup);
 // router.route('/group-add').put(protect, addToGroup);
