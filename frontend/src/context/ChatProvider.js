70817a1b61da1c94c0662530b1d7eaa4241b4bfe
Bibek Bhusal <iambibker@gmail.com>
2023-09-25 23:55:12 +0545

    Creating Context in React

---
+++ frontend/src/context/ChatProvider.js
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+const ChatContext = createContext()
+
+const ChatProvider= ({children})=> {
+    return <ChatContext.Provider>{children}</ChatContext.Provider>
+};
+
+export default ChatProvider;
