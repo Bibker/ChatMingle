330c946902fe8e100da1e2598165d27145b11041
Bibek Bhusal <iambibker@gmail.com>
2023-11-01 13:23:28 +0545

    ChatBox UI Completed

--- frontend/src/Components/MyChats.js
+++ frontend/src/Components/MyChats.js
@@ -7,7 +7,7 @@ import { AddIcon } from '@chakra-ui/icons';
 import { getSender } from '../config/ChatLogics';
 import GroupChatModal from './miscellaneous/GroupChatModal';
 
-const MyChats = () => {
+const MyChats = ({fetchAgain}) => {
   const [loggedUser, setLoggedUser] = useState();
   const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
   const toast = useToast();
@@ -38,7 +38,7 @@ const MyChats = () => {
   useEffect(() => {
     setLoggedUser(JSON.parse(localStorage.getItem("user")));
     fetchChats();
-  }, []);
+  }, [fetchAgain]);
 
   return (
    <Box
