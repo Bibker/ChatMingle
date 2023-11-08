2b8c58208f3d8c171dac4cf1b7f8d4b81e2bc89b
Bibek Bhusal <iambibker@gmail.com>
2023-11-08 22:07:32 +0545

    Fetch All Message Frontend Logic Completed

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -5,6 +5,6 @@ const router= express.Router();
 
 
 router.route('/').post(protect, sendMessage)
-router.route(':chatId/').get(protect, allMessages)
+router.route('/:chatId/').get(protect, allMessages)
 
 module.exports = router;
